89f8e4a281e629af2356274b69aecfeba01bee74
fleshi3 <markuleh@gmail.com>
2019-04-13 22:43:21 +1000

    Updates

---
+++ precache-manifest.f1e2d60ecf3af41edbb3fc13dd63f698.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "df888ec8d714e87f90f2",
+    "url": "/calc-react-app/static/css/main.84d65db6.chunk.css"
+  },
+  {
+    "revision": "df888ec8d714e87f90f2",
+    "url": "/calc-react-app/static/js/main.289af4c2.chunk.js"
+  },
+  {
+    "revision": "6eb4256a11f04614155b",
+    "url": "/calc-react-app/static/js/runtime~main.3974e04c.js"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/calc-react-app/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "16ae944240cc6a714b3b2d7b181fc3c3",
+    "url": "/calc-react-app/index.html"
+  }
+];
